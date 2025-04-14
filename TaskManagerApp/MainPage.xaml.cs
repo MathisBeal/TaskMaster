b99026d97c1b5245047264be7c8fc5e9873b204f
estebientrkl <esteban.vigouroux@eut.uca.fr>
2025-04-14 17:16:46 +0200

    Ajout page login (fonctionnel

--- TaskManagerApp/MainPage.xaml.cs
+++ TaskManagerApp/MainPage.xaml.cs
@@ -18,40 +18,51 @@ public partial class MainPage : ContentPage
         base.OnAppearing();
         Debug.WriteLine("OnAppearing called in MainPage");
 
-        // Vérifie si l'utilisateur est connecté avant de charger la liste des utilisateurs
         if (isLoggedIn)
         {
-            var users = await _userService.ObtenirUtilisateursAsync();
-            Debug.WriteLine($"Number of users retrieved: {users.Count}");
-            foreach (var user in users)
-            {
-                Debug.WriteLine($"User: {user.nom} {user.prenom}");
-            }
-            listeUtilisateurs.ItemsSource = users;
+            await ChargerUtilisateurs();
         }
         else
         {
-            // Affiche un message pour rappeler à l'utilisateur de se connecter
             Debug.WriteLine("Veuillez vous connecter pour voir les utilisateurs.");
         }
     }
 
-    private void OnLoginClicked(object sender, EventArgs e)
+    private async Task ChargerUtilisateurs()
+    {
+        var users = await _userService.ObtenirUtilisateursAsync();
+        Debug.WriteLine($"Number of users retrieved: {users.Count}");
+        foreach (var user in users)
+        {
+            Debug.WriteLine($"User: {user.nom} {user.prenom}");
+        }
+        listeUtilisateurs.ItemsSource = users;
+    }
+
+    private async Task CheckLogAsync(string email, string password)
+    {
+        var users = await _userService.ObtenirUtilisateursAsync();
+        Debug.WriteLine($"Number of users retrieved: {users.Count}");
+
+        foreach (var user in users)
+        {
+            if (email == user.email && password == user.password)
+            {
+                isLoggedIn = true;
+                await DisplayAlert("Succès", "Connexion réussie!", "OK");
+                await ChargerUtilisateurs();
+                return;
+            }
+        }
+
+        await DisplayAlert("Erreur", "Email ou mot de passe incorrect.", "OK");
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
     {
         string email = EmailEntry.Text;
         string password = PasswordEntry.Text;
 
-        // Exemple simple de validation
-        if (email == "admin@example.com" && password == "password123")
-        {
-            isLoggedIn = true;
-            DisplayAlert("Succès", "Connexion réussie!", "OK");
-            // Recharge la liste des utilisateurs après la connexion réussie
-            OnAppearing();
-        }
-        else
-        {
-            DisplayAlert("Erreur", "Email ou mot de passe incorrect.", "OK");
-        }
+        await CheckLogAsync(email, password);
     }
 }
